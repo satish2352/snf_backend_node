367b1db1871ccf5ca675c481e8e86ba8a4cd920b
Rajendra Fodase <rajendrafodase61@gmail.com>
2024-05-25 16:36:11 +0530

    changes done in app fie

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
-mongoose.connect("mongodb+srv://snfbackend:7SkTLYORShY2XQ1B@cluster0.bsuehxw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/SNF_backend")
+mongoose.connect("mongodb+srv://snfbackend:7SkTLYORShY2XQ1B@cluster0.bsuehxw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/snf_backend")
     .then(() => {
         console.log("connet to database successfully")
     })
